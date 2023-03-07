ea3db0f356787496ea4ae5765f0d4493e96668d9
Leandro Gonçalves <goncalvesleandro68@gmail.com>
2023-03-07 13:34:44 -0300

    Finalizado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,10 +13,18 @@ fun main() {
         println(i)
     }
     maiorDif(v)
+    ordemCres(v)
 }
 fun maiorDif(v : IntArray) : Unit{
     var maior = v.max()
     var menor = v.min()
     var dif = maior - menor
     println("A maior diferença é: ${dif}")
+}
+fun ordemCres(v: IntArray) : Unit{
+    var cres = false
+    for (i in 1 until v.size){
+        cres = if (v[i] > v[i - 1]) true else false
+    }
+    println("O vetor é crescente: ${if (cres == true) "Sim" else "Não"}")
 }
